9ce35a242aac09b36f84e20da7a132a2e7d08033
MUTO Takaaki 武藤 隆晃 <is0582pr@ed.ritsumei.ac.jp>
2022-02-26 16:44:32 +0900

    ver1.1 SeSliderの調整

--- Assets/DtmControl/Script/DtmSlider/SeSlider.cs
+++ Assets/DtmControl/Script/DtmSlider/SeSlider.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class SeSlider : DtmSlider
+public class SeSlider : DtmSlider,  IPointerUpHandler
 {
     protected override void SetUp()
     {
         dtm = SeManager.instance;
 
         slider.value = SeManager.volume;
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        SeManager.instance.PlayOneShot(SeName.click);
     }
 }
